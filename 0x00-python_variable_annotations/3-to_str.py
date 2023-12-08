ebd3c47a6d0002644b18b4863d2691b1a891fc53
Tassuo7 <abdelhamid.jack@gmail.com>
2023-12-07 17:12:54 -0800

    type-annotated functions

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns the string representation of the float
+    """
+    return str(n)
---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type-annotated function
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    returns a function that multiplies a float by multiplier
+    """
+    return lambda x: x * multiplier
